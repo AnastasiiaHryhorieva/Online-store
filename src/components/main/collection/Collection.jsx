dcd012e54b667098754298d316c90a3f44de65e3
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-10-22 14:30:25 +0300

    created a philosophy section

--- src/components/main/collection/Collection.jsx
+++ src/components/main/collection/Collection.jsx
@@ -3,7 +3,7 @@ import Button from "../../shared/Button";
 
 const Collection = () => {
   return (
-    <div className="bg-collection-bg bg-no-repeat bg-cover">
+    <div className="bg-collection-bg bg-no-repeat bg-cover mb-[150px]">
       <section className="pt-[400px] flex-center flex-col">
         <p className="text-100 text-stroke__black mb-9 text-center uppercase max-w-2xl pt-4">
           SS'2023 Collection
