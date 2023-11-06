0e7bf766533462e76b64b26e7d808d24f9fcb2bc
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-11-06 22:57:12 +0200

    connect cms

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import { useQuery } from "graphql-hooks";
+
+const HOMEPAGE_QUERY = `query MyQuery {
+  allProducts {
+    title
+  }
+}`;
 
 function App() {
+  const { loading, error, data } = useQuery(HOMEPAGE_QUERY);
+  console.log(data);
   return (
     <>
       <Header />
       <Main />
       <Footer />
+      {loading ? JSON.stringify(loading) : JSON.stringify(data)}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import React, { Suspense } from "react";
+import React from "react";
 
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { GraphQLClient, ClientContext } from "graphql-hooks";
 
 import "./index.css";
 import router from "./routes";
 
+const client = new GraphQLClient({
+  url: "https://graphql.datocms.com/",
+  headers: {
+    Authorization: "Bearer 90c4dc302cbe36d790dcec6ca3929c",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ClientContext.Provider value={client}>
+      <RouterProvider router={router} />
+    </ClientContext.Provider>
   </React.StrictMode>
 );
