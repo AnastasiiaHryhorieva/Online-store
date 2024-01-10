9cf95931778240826ca9e25562f1e64d5c396cf1
vadrille <109307947+vadrille12@users.noreply.github.com>
2024-01-10 21:48:49 +0200

    some fixes

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Button = () => {
   return (
-    <button className="px-14 py-3 border-2 mb-9">
-      <span className="lg">Переглянути</span>
+    <button className="px-14 py-3 border-2 mb-9 bg-black">
+      <span className="lg text-white">Переглянути</span>
     </button>
   );
 };
