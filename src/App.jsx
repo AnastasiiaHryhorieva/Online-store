de49fd6c79806cb509dc20ba3f765f814b4b0e05
Yaroslav Lebedenko <nailheart4@gmail.com>
2024-01-19 19:10:40 +0200

    improved collection section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import Home from "src/pages/home/Home";
-import Header from "./components/header/Header";
 
-function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
-}
+const App = () => {
+  return <Home />;
+};
 
 export default App;
--- src/pages/home/main/collection/Collection.jsx
+++ src/pages/home/main/collection/Collection.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-
-import Button from "src/components/shared/Button";
+import { NavLink } from "react-router-dom";
 
 const Collection = () => {
   return (
-    <div className="bg-collection-bg bg-no-repeat bg-cover mb-[150px] bg-center">
-      <section className="pt-[400px] flex-center flex-col">
-        <p className="text-100 text-stroke__black mb-9 text-center uppercase max-w-2xl pt-4">
+    <section className="bg-collection-bg bg-cover bg-center bg-no-repeat pb-[35px] pt-[270px] sm:pt-[320px] md:pt-[400px]">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-stroke-black mb-9 max-w-2xl text-center text-[50px] font-bold uppercase leading-tight text-transparent sm:text-[70px] md:text-[100px]">
           SS'2023 Collection
-        </p>
-        <Button />
-      </section>
-    </div>
+        </h1>
+        <NavLink
+          className="flex min-w-[226px] items-center justify-center border border-black bg-black px-4 pb-3 pt-2 text-[20px] leading-normal text-white duration-300 hover:bg-transparent hover:text-black"
+          to="#"
+        >
+          Переглянути
+        </NavLink>
+      </div>
+    </section>
   );
 };
 
