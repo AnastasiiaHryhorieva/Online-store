d9ccd1054e82bde8718c8024bb58c14cb5976519
Yaroslav Lebedenko <nailheart4@gmail.com>
2024-01-20 17:06:02 +0200

    + responsive philosophy section

--- src/components/home/philosophy/philosophy.jsx
+++ src/components/home/philosophy/philosophy.jsx
@@ -5,30 +5,34 @@ const Philosophy = () => {
   const flowersImage = useImage("white_red_pink_flowers");
 
   return (
-    <section className="mx-auto mb-[150px] max-w-[1140px]">
-      <div className="flex gap-36">
-        <div className="max-w-[390px]">
-          <section>
-            <p className="text-40 mb-[40px] uppercase">Наша філософія</p>
-            <p className="text-18 mb-[44px]">
-              Наш одяг створений для того, щоб ти незалежно від настрою чи
-              ситуації, могла виглядати неперевершено. Могла бути собою, бо Ти і
-              є неперевершена. Жити своєю роботою, любити її до кінчиків
-              пальців, такий наш підхід
-            </p>
-          </section>
-          <img src={flowersImage?.src} alt={flowersImage?.alt} />
+    <section className="py-[75px] md:py-[150px]">
+      <div className="container grid gap-y-10 md:grid-cols-[35%_55%] md:gap-x-[10%]">
+        <div>
+          <h2 className="mb-[40px] text-title uppercase">Наша філософія</h2>
+          <p className="text-base">
+            Наш одяг створений для того, щоб ти незалежно від настрою чи
+            ситуації, могла виглядати неперевершено. Могла бути собою, бо Ти і є
+            неперевершена. Жити своєю роботою, любити її до кінчиків пальців,
+            такий наш підхід
+          </p>
+          <img
+            className="mt-[40px]"
+            src={flowersImage?.src}
+            alt={flowersImage?.alt}
+          />
         </div>
-        <div className="flex max-w-[610px] flex-col justify-between">
-          <img src={girlImage?.src} alt={girlImage?.alt} className="w-{610}" />
-          <section>
-            <p className="text-18 max-w-[500px]">
-              Майже всі речі з наших колекцій легко поєднуються між собою за
-              стилем та кольоровою гамою. Ви можете міксувати різні силуети і
-              кольори. Комфортний образ зробить твій день кращим, а тебе
-              впевненішою. Один день, але різна ти, з Мocko можливо все
-            </p>
-          </section>
+        <div className="flex flex-col gap-y-10">
+          <img
+            className="mb-auto max-md:order-1"
+            src={girlImage?.src}
+            alt={girlImage?.alt}
+          />
+          <p className="max-w-[500px] text-base">
+            Майже всі речі з наших колекцій легко поєднуються між собою за
+            стилем та кольоровою гамою. Ви можете міксувати різні силуети і
+            кольори. Комфортний образ зробить твій день кращим, а тебе
+            впевненішою. Один день, але різна ти, з Мocko можливо все
+          </p>
         </div>
       </div>
     </section>
