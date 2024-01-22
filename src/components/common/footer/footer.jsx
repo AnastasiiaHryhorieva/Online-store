621284b03080267e4b671a896e9c6fecc683875b
Yaroslav Lebedenko <nailheart4@gmail.com>
2024-01-22 11:30:34 +0200

    * footer

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -1,86 +1,141 @@
-import React from "react";
-
 import { Link } from "react-router-dom";
 
-import useLogo from "src/hooks/graphQL/useLogo";
-import useSocials from "src/hooks/graphQL/useSocials";
+import { Logo } from "src/components/common/logo/logo";
+import { Icon } from "src/components/common/icon/icon";
 
 const Footer = () => {
-  const socials = useSocials();
-
-  const data = socials;
-  const socialsData = data?.allUploads;
-
-  const logo = useLogo("secondary");
-
   return (
-    <footer className="bg-black pt-[120px] text-white">
-      <div className="m-auto mb-[50px] flex max-w-[1046px] flex-wrap justify-around gap-[150px]">
-        <div>
-          <Link to="/">
-            <img src={logo?.responsiveImage?.src} alt={logo?.alt} />
+    <footer className="bg-black pb-4 pt-[120px] text-white">
+      <div className="container">
+        <div className="mb-[120px] grid gap-16 max-md:mb-16 max-md:justify-center max-md:justify-items-center md:grid-cols-[min-content_1fr]">
+          <div className="">
+            <Logo />
+
+            <div className="mt-6 flex gap-4 md:mt-16">
+              <Link
+                className="duration-200 hover:text-[--green] focus-visible:text-[--green]"
+                to="#"
+              >
+                <Icon size={40} name="instagram" />
+              </Link>
+              <Link
+                className="duration-200 hover:text-[--green] focus-visible:text-[--green]"
+                to="#"
+              >
+                <Icon size={40} name="twitter" />
+              </Link>
+              <Link
+                className="duration-200 hover:text-[--green] focus-visible:text-[--green]"
+                to="#"
+              >
+                <Icon size={40} name="facebook" />
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap gap-10 sm:max-md:justify-center md:ml-auto md:gap-x-16 lg:gap-x-28">
+            <div>
+              <h2 className="mb-6 text-[20px] font-bold uppercase">Контакти</h2>
+              <Link
+                className="link-hover mb-8 inline-block text-base leading-[normal] opacity-70"
+                to="tel:+380970458414"
+              >
+                +380 97 045 8414
+              </Link>
+              <ul className="space-y-4 opacity-70">
+                <li>вул. Бойківська 1, Львів</li>
+                <li>пн-сб: 11:00-19:00</li>
+                <li>нд: 12:00-18:00</li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-[20px] font-bold uppercase">Магазин</h2>
+              <ul className="space-y-4 opacity-70">
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="/suits&bodysuits"
+                  >
+                    Костюми та боді
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="/outerwear"
+                  >
+                    Верхній одяг
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="/underwear"
+                  >
+                    Нижній одяг
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="/new"
+                  >
+                    Новинки
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="/dresses"
+                  >
+                    Сукні
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-[20px] font-bold uppercase">
+                Наш сервіс
+              </h2>
+              <ul className="space-y-4 opacity-70">
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="#"
+                  >
+                    Доставка та оплата
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="#"
+                  >
+                    Вхід/Реєстрація
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="link-hover text-base leading-[normal]"
+                    to="#"
+                  >
+                    Відгуки
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-center gap-x-10 gap-y-4 opacity-70 max-md:flex-col">
+          <Link className="link-hover" to="#">
+            Політика конфіденційності
+          </Link>
+          <p className="max-md:-order-1">@ 2023 All right reserved</p>
+          <Link className="link-hover" to="#">
+            Користувацька згода
           </Link>
         </div>
-        <div className="flex gap-[126px]">
-          <section className="flex flex-col gap-6">
-            <h4 className="text-[20px] font-bold uppercase">Контакти</h4>
-            <ul className="flex flex-col gap-4 opacity-70">
-              <li>вул. Бойківська 1, Львів</li>
-              <li>пн-сб: 11:00-19:00</li>
-              <li>нд: 12:00-18:00</li>
-            </ul>
-          </section>
-          <section className="flex flex-col gap-6">
-            <h4 className="text-[20px] font-bold uppercase">Магазин</h4>
-            <ul className="flex flex-col gap-4 opacity-70">
-              <li>
-                <Link to="/suits&bodysuits">Костюми та боді</Link>
-              </li>
-              <li>
-                <Link to="/outerwear">Верхній одяг</Link>
-              </li>
-              <li>
-                <Link to="/underwear">Нижній одяг</Link>
-              </li>
-              <li>
-                <Link to="/new">Новинки</Link>
-              </li>
-              <li>
-                <Link to="/dresses">Сукні</Link>
-              </li>
-            </ul>
-          </section>
-          <section className="flex flex-col gap-6">
-            <h4 className="text-[20px] font-bold uppercase">Наш сервіс</h4>
-            <ul className="flex flex-col gap-4 opacity-70">
-              <li>
-                <Link to="#">Доставка та оплата</Link>
-              </li>
-              <li>
-                <Link to="#">Вхід/Реєстрація</Link>
-              </li>
-              <li>
-                <Link to="#">Відгуки</Link>
-              </li>
-            </ul>
-          </section>
-        </div>
-      </div>
-      <div className="m-auto mb-[132px]  flex max-w-[1046px] gap-4">
-        {socialsData?.map((icon) => (
-          <Link to="#" key={icon.id}>
-            <img src={icon.url} alt={icon.alt} />
-          </Link>
-        ))}
-      </div>
-      <div className="flex-center gap-10 pb-5">
-        <p className="opacity-70">
-          <Link to="#">Політика конфіденційності</Link>
-        </p>
-        <p className="opacity-70">@ 2023 All right reserved</p>
-        <p className="opacity-70">
-          <Link to="#">Користувацька згода</Link>
-        </p>
       </div>
     </footer>
   );
--- src/components/common/logo/logo.jsx
+++ src/components/common/logo/logo.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
 
+import { cn } from "src/helpers/helpers";
 import LogoIcon from "src/assets/images/logo.svg?react";
 
 const Logo = ({ className }) => {
   return (
-    <NavLink className={className} to="/">
+    <NavLink className={cn("inline-block", className)} to="/">
       <LogoIcon
         className="fill-current"
         width="145"
